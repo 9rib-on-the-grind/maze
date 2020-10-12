498cef7f203ceeaa9fc18bc35dc1a3995a58d974
electropavuk <71542379+electropavuk@users.noreply.github.com>
2020-10-12 19:48:45 +0300

    first commit

--- main.py
+++ main.py
@@ -6,15 +6,15 @@ sys.setrecursionlimit(10**5)
 
 
 m = maze(55, 33)		
-m.generate(0.03)		# draw generation step with 0.03 sec delay
-m.solve(0.3)			# draw solving step with 0.3 sec delay
+m.generate(0.03)        # draw generation step with 0.03 sec delay
+m.solve(0.3)            # draw solving step with 0.3 sec delay
 
 
-# m = maze(55, 33)		# create maze 55 x 33 cells
+# m = maze(55, 33)      # create maze 55 x 33 cells
 # m.generate()
-# m.show()				# display maze
-# 						# ! maze has to be generated
+# m.show()              # display maze
+#                       # ! maze has to be generated
 # m.solve()				
-# m.show(m.solution)	# show maze with solution
-# 						# ! maze has to be solved
-# print(m.solution)		# print text solution
+# m.show(m.solution)    # show maze with solution
+#                       # ! maze has to be solved
+# print(m.solution)     # print text solution
